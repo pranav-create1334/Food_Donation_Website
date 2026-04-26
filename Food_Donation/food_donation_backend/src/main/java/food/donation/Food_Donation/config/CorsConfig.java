4ff9d66b62f150758c82999b911eca81aa3dc705
Auto Deploy <auto@deploy.local>
2026-04-26 20:18:36 +0530

    error

--- Food_Donation/food_donation_backend/src/main/java/food/donation/Food_Donation/config/CorsConfig.java
+++ Food_Donation/food_donation_backend/src/main/java/food/donation/Food_Donation/config/CorsConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class CorsConfig {
 
@@ -17,6 +16,7 @@ public class CorsConfig {
                         .allowedOrigins(
                             "https://food-donation-website-ai8d7gyub-prs-projects-fa9db43c.vercel.app",
                             "https://food-donation-website-ai8d7gyub-prs-projects-fa9db43c-git-main-prs-projects.vercel.app",
+                            "https://food-donation-website-ai8d7gyub-prs-projects-fa9db43c.vercel.app",
                             "https://food-donation-website-amqz.onrender.com",
                             "http://localhost:3000",
                             "http://localhost:5173",
